38aa2b8fba915357c647d511298513822f70a856
mslegion <saifmd18@gmail.com>
2021-06-25 12:47:28 +0530

    adding docker app deploy scripts

--- deployment/main.py
+++ deployment/main.py
@@ -6,11 +6,12 @@ try:
     client = ssh.SSHClient(key_pair_name, host_name, user_name)
     client.connect()
 
-    setup_handler.update_get_tools(client)
-    setup_handler.install_docker(client)
+    # setup_handler.update_get_tools(client)
+    # setup_handler.install_docker(client)
     # setup_handler.delete_directory(client, "sitevisitor")
-    setup_handler.clone_git_repository(client, "https://github.com/mslegion/sitevisitor.git")
-    setup_handler.run_docker_app(client, "sitevisitor")
+    # setup_handler.clone_git_repository(client, "https://github.com/mslegion/sitevisitor.git")
+    # set up info token in remote
+    # setup_handler.run_docker_app(client, "sitevisitor")
 
     client.close()
 except Exception as e:
