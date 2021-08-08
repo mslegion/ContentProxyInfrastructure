c6c58e97bfd0ba125fc5f1823dbbe89807c25b0a
Mohammad Saif Sabir <mohammad.sabir@grabtaxi.com>
2021-08-08 22:44:02 +0530

    ohio

--- deployment/main.py
+++ deployment/main.py
@@ -6,10 +6,10 @@ try:
     client = ssh.SSHClient(key_pair_name, host_name, user_name)
     client.connect()
 
-    setup_handler.update_get_tools(client)
-    setup_handler.clone_git_repository(client, "https://github.com/mslegion/BasicFilterProxy.git")
-    setup_handler.install_maven(client)
-    setup_handler.run_application(client, "BasicFilterProxy")
+    # setup_handler.update_get_tools(client)
+    # setup_handler.clone_git_repository(client, "https://github.com/saifsabir97/Proxy-Server.git")
+    # setup_handler.install_maven(client)
+    setup_handler.run_application(client, "Proxy-Server")
     # setup_handler.kill_application(client)
 
     client.close()
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   type    = string
-  default = "Germany"
+  default = "Ohio"
 }
 
 variable "proxy_tag" {
